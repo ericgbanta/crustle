d024d1ed9b92b328b4c89cc932df3441d78087cd
Eric Banta <ericgbanta@github.com>
2023-08-14 00:46:48 -0700

    setup routes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
+use dioxus_router::prelude::*;
+
+// Routes
+#[rustfmt::skip]
+#[derive(Clone, Debug, PartialEq, Routable)]
+enum Route {
+    #[route("/")]
+    Home {},
+    #[route("/pokedex")]
+    Pokedex {},
+    #[route("/contact")]
+    Contact {},
+    #[route("/:..route")]
+    PageNotFound {
+        route: Vec<String>,
+    },
+}
 
 // Constants
 const GITHUB_IMG: &'static str = wasm_or_else("github.svg", "public/github.svg");
@@ -10,33 +27,74 @@ fn main() {
     dioxus_web::launch(App);
 }
 
-// App component
-pub fn App(cx: Scope) -> Element {
+fn App(cx: Scope) -> Element {
+    render! {
+        Router::<Route> { }
+    }
+}
+
+// Home component
+pub fn Home(cx: Scope) -> Element {
     cx.render(rsx! {
         div {
             class: "relative flex flex-col min-h-screen",
-            Header {},
+            Header {name:"Pokérust".into()},
             About {},
         }
     })
 }
 
+// Pokedex component
+pub fn Pokedex(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            class: "relative flex flex-col min-h-screen",
+            Header {name:"Pokédex".into()},
+            About {},
+        }
+    })
+}
+
+// Contact component
+pub fn Contact(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            class: "relative flex flex-col min-h-screen",
+            Header {name:"Contact".into()},
+            About {},
+        }
+    })
+}
+
+#[inline_props]
+fn PageNotFound(cx: Scope, route: Vec<String>) -> Element {
+    render! {
+        h1 { "Page not found" }
+        p { "We are terribly sorry, but the page you requested doesn't exist." }
+        pre {
+            color: "red",
+            "log:\nattemped to navigate to: {route:?}"
+        }
+    }
+}
+
 // Header component
-pub fn Header(cx: Scope) -> Element {
+#[inline_props]
+pub fn Header(cx: Scope, name: String) -> Element {
     cx.render(rsx! {
         header {
             class: "flex justify-between items-center p-8",
             h1 {
                 class: "text-4xl",
-                "pokérust"
+                "{name}"
             },
             div {
                 class: "flex items-center",
                 nav {
                     class: "hidden md:flex",
-                    a { class: "px-4 py-2", href: "/", "Home" }
-                    a { class: "px-4 py-2", href: "/pokedex", "Pokedex" }
-                    a { class: "px-4 py-2", href: "/contact", "Contact" }
+                    Link { class: "px-4 py-2", to: "/", "Home" }
+                    Link { class: "px-4 py-2", to: "/pokedex", "Pokédex" }
+                    Link { class: "px-4 py-2", to: "/contact", "Contact" }
                 }
             }
         }
@@ -47,7 +105,7 @@ pub fn Header(cx: Scope) -> Element {
 pub fn About(cx: Scope) -> Element {
     cx.render(rsx! {
         p {
-            class: "mt-auto p-8 flex items-center italic",
+            class: "mt-auto p-8 flex items-center italic text-xs",
             a {
                 href: "https://github.com/ericgbanta/pokerust/",
                 target: "_blank",
