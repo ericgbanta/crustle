ae0705b7f01bd6f46d44a4ae4665dff7ccd1af6b
Eric Banta <ericgbanta@github.com>
2023-08-13 23:12:05 -0700

    add tailwind and dioxus configs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,71 @@
 #![allow(non_snake_case)]
-// import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
+
 use dioxus::prelude::*;
 
+// Constants
+const GITHUB_IMG: &'static str = wasm_or_else("github.svg", "public/github.svg");
+
+// Entry point
 fn main() {
-    // launch the web app
     dioxus_web::launch(App);
 }
 
-// create a component that renders a div with the text "Hello, world!"
-fn App(cx: Scope) -> Element {
+// App component
+pub fn App(cx: Scope) -> Element {
     cx.render(rsx! {
         div {
-            "Hello, world!!"
+            class: "relative flex flex-col min-h-screen",
+            Header {},
+            About {},
         }
     })
+}
+
+// Header component
+pub fn Header(cx: Scope) -> Element {
+    cx.render(rsx! {
+        header {
+            class: "flex justify-between items-center p-8",
+            h1 {
+                class: "text-4xl",
+                "pokérust"
+            },
+            div {
+                class: "flex items-center",
+                nav {
+                    class: "hidden md:flex",
+                    a { class: "px-4 py-2", href: "/", "Home" }
+                    a { class: "px-4 py-2", href: "/pokedex", "Pokedex" }
+                    a { class: "px-4 py-2", href: "/contact", "Contact" }
+                }
+            }
+        }
+    })
+}
+
+// About component
+pub fn About(cx: Scope) -> Element {
+    cx.render(rsx! {
+        p {
+            class: "mt-auto p-8 flex items-center italic",
+            a {
+                href: "https://github.com/ericgbanta/pokerust/",
+                target: "_blank",
+                img {
+                    class: "w-4 sm:w-8 align-middle mr-2",
+                    src: GITHUB_IMG,
+                }
+            }
+            " An Open Source project to create a Pokédex using Rust & Dioxus."
+        }
+    })
+}
+
+// Utility function
+const fn wasm_or_else<'a, T: ?Sized>(then: &'a T, _else: &'a T) -> &'a T {
+    if cfg!(target_family = "wasm") {
+        then
+    } else {
+        _else
+    }
 }
